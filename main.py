74015015ed503c6814f7299ebffe744c981a44f4
Patryk Kotarski <73948640+Patrykkotarski812@users.noreply.github.com>
2021-02-25 17:05:35 +0100

    dodanie zmiennej a

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-import sys as system
+from math import *
+a = 10
+b = 5
+c = 2
 
-print(system.version)
-a = 2
+dodawanie = a + b
+mnozenie = a * c
+dzielenie = a/b
+dzielenie_calkowite = b // c
+potega = c ** b
+print(dodawanie)
+print(mnozenie, dzielenie)
+print(dzielenie)
+print(dzielenie_calkowite)
+print(potega)
+print(pow(8, 2))
+a += c
 print(a)
+
+print(sqrt(2))
+print(pi)
+
+
+
+print (u'inżynieria systemów informatycznych')
+print ('wynik dzialania jest rowny a=%(zm)d' % {'zm':12})
+
+napis = 'isi'
+grupa = 2
+print('specjalizacja %s, grupa %d' %(napis, grupa))
+
+a = 5
+b = 3
+z = 5 - 3
+print('Wynik dzialania %(z1)d - %(z2)d = %(z3)d' %{'z1': a, 'z2':b, 'z3':z})
+
